627a1a0d9c95ac6094582f6ba5ee479dfb06d79b
ramzi <ramzi@itinerix.(none)>
2010-12-11 14:53:32 +0100

    un ptit message de debug pour ns aider

--- TP/serveur/message.c
+++ TP/serveur/message.c
@@ -44,6 +44,10 @@ int message_connect_2_server(char *ip, uint64_t port)
 	
 	shutdown(sockServer,SHUT_RDWR);
 	
+	printf("debug info client\n");
+	afficherIdentConnexion(SERVEUR.suivServeur);
+	printf("fin debug");
+	
 	new_socket=connect2server(inet_ntoa(SERVEUR.suivServeur->identifiant.sin_addr),ntohs(SERVEUR.suivServeur->identifiant.sin_port));
 	envoyerOrigine(FROM_SERVEUR,new_socket);
 	envoyerTypeMessage(CONNECT,new_socket);
